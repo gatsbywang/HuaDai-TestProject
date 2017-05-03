dc17258038d700cb0f039a385791e25f6397d6ba
花歹 <gatsbywang@126.com>
2017-05-03 21:56:11 +0800

    添加builder模式 自定义头部

---
+++ absnavigationbardemo/src/main/java/myapplication/absnavigationbardemo/INavigationBar.java
@@ -0,0 +1,23 @@
+package myapplication.absnavigationbardemo;
+
+/**
+ * Builder设置模式，
+ * Created by 花歹 on 2017/5/2.
+ * Email:   gatsbywang@126.com
+ */
+
+public interface INavigationBar {
+
+    /**
+     * 绑定资源Id
+     *
+     * @return
+     */
+    public int bindLayoutId();
+
+
+    /**
+     * 构建NavigationBar
+     */
+    public void applyView();
+}
---
+++ absnavigationbardemo/src/main/java/myapplication/absnavigationbardemo/MainActivity.java
@@ -0,0 +1,27 @@
+package myapplication.absnavigationbardemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new DefaultNavigationBar.Builder(this)
+                .setTitle("MainActivityTitle")
+                .setRightText("右边导航")
+                .setRightClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "右边导航", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .build();
+    }
+
+}
